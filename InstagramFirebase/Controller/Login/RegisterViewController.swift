6d802c41ce3b0debb00f985190b0f301f72d9274
Gabor Sornyei <info@sornyei.com>
2019-06-03 23:59:38 +0200

    Profile image select

--- InstagramFirebase/Controller/Login/RegisterViewController.swift
+++ InstagramFirebase/Controller/Login/RegisterViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 import Firebase
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var addPhotoButton: UIButton! {
+        didSet {
+            
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func addPhotoButtonTapped() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            print("original")
+            addPhotoButton.setImage(originalImage, for: .normal)
+        } else if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            addPhotoButton.setImage(editedImage, for: .normal)
+        }
+        addPhotoButton.layer.cornerRadius = addPhotoButton.bounds.width / 2
+        addPhotoButton.layer.masksToBounds = true
+        addPhotoButton.layer.borderColor = UIColor.black.cgColor
+        addPhotoButton.layer.borderWidth = 1.5
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
 
     @IBAction func signUpButtonTapped() {
@@ -29,12 +60,6 @@ class RegisterViewController: UIViewController {
             }
             if let user = firUser?.user {
                 let dictionary = ["username": username, "email": email]
-//                Firestore.firestore().collection("users").addDocument(data: dictionary, completion: { (error) in
-//                    if let error = error {
-//                        print("Error in saving user:", error.localizedDescription)
-//                        return
-//                    }
-//                })
                 Firestore.firestore().collection("users").document(user.uid).setData(dictionary, completion: { (error) in
                     if let error = error {
                         print("Error in saving user:", error.localizedDescription)
